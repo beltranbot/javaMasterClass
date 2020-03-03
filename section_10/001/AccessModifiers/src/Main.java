ccce99783d52330e465568b392d16ca814a61e16
beltranbot <beltranbot@gmail.com>
2020-03-02 22:15:56 -0500

    finished section 10, started section 11

---
+++ section_10/001/AccessModifiers/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Account account = new Account("Tim");
+        account.deposit(1000);
+        account.withdraw(500);
+        account.withdraw(-200);
+        account.deposit(-20);
+        account.calculateBalance();
+
+        System.out.println("Balance on account is " + account.getBalance());
+
+    }
+}
