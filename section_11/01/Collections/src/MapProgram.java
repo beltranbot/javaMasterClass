283a3e76da9a2a7a201f31c9c30956f36a7801c3
beltranbot <beltranbot@gmail.com>
2020-05-10 22:46:42 -0500

    157. Map continued and adventure game

--- section_11/01/Collections/src/MapProgram.java
+++ section_11/01/Collections/src/MapProgram.java
@@ -24,6 +24,19 @@ public class MapProgram {
         System.out.println(languages.get("Java"));
         System.out.println("=====================");
 
+//        languages.remove("Lisp");
+        if (languages.remove("Algol", "a family of algorithmic languages")) {
+            System.out.println("Algol Removed");
+        } else {
+            System.out.println("Algol not removed, key/value pair not found");
+        }
+        if (languages.replace("Lisp", "Therein lies madness", "a functional programming language  with imperative features")) {
+            System.out.println("Lisp replaced");
+        } else {
+            System.out.println("Lisp was not replaced");
+        }
+//        System.out.println(languages.replace("Lisp", "a functional programming language  with imperative features"));
+        System.out.println(languages.replace("Scala", "this will not be added"));
         for (String key : languages.keySet()) {
             System.out.println(key + " : " + languages.get(key));
         }
