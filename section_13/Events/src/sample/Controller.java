704d19ae6a8570d1243806dccb8e10ecaaa990ed
beltranbot <beltranbot@gmail.com>
2020-05-27 22:18:38 -0500

    193. events continued

--- section_13/Events/src/sample/Controller.java
+++ section_13/Events/src/sample/Controller.java
@@ -1,6 +1,8 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -8,9 +10,31 @@ public class Controller {
     // variable has to have the same name that the id in the fxml
     @FXML
     private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
 
     @FXML
-    public void onButtonClicked() {
-        System.out.println("Hello, " + nameField.getText());
+    public void initialize() {
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+
+    @FXML
+    public void onButtonClicked(ActionEvent e) {
+        if (e.getSource().equals(helloButton)) {
+            System.out.println("Hello, " + nameField.getText());
+        } else if (e.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + nameField.getText());
+        }
+    }
+
+    @FXML
+    public void handleKeyReleased() {
+        String text =nameField.getText();
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disableButtons);
+        byeButton.setDisable(disableButtons);
     }
 }
