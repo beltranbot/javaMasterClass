b37536e6d6837360084659df0fc8698f1e453ed8
beltranbot <beltranbot@gmail.com>
2020-05-27 22:33:49 -0500

    194. ui thread

--- section_13/Events/src/sample/Controller.java
+++ section_13/Events/src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -14,6 +15,8 @@ public class Controller {
     private Button helloButton;
     @FXML
     private Button byeButton;
+    @FXML
+    private CheckBox ourCheckbox;
 
     @FXML
     public void initialize() {
@@ -28,13 +31,30 @@ public class Controller {
         } else if (e.getSource().equals(byeButton)) {
             System.out.println("Bye, " + nameField.getText());
         }
+
+        try {
+            Thread.sleep(10000);
+        } catch(InterruptedException event) {
+            // we don't care about this
+        }
+
+        if (ourCheckbox.isSelected()) {
+            nameField.clear();
+            helloButton.setDisable(true);
+            byeButton.setDisable(true);
+        }
     }
 
     @FXML
     public void handleKeyReleased() {
-        String text =nameField.getText();
+        String text = nameField.getText();
         boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
         helloButton.setDisable(disableButtons);
         byeButton.setDisable(disableButtons);
+    }
+
+    @FXML
+    public void handleChange() {
+        System.out.println("The checkbox is " + (ourCheckbox.isSelected() ? "checked" : "not checked"));
     }
 }
