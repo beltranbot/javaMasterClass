4adf3c141fb92f03273b4356ead48d8ac4e38994
beltranbot <beltranbot@gmail.com>
2020-05-28 21:45:57 -0500

    199. formatting dates

--- section_13/TodoList/src/com/beltranbot/todolist/Controller.java
+++ section_13/TodoList/src/com/beltranbot/todolist/Controller.java
@@ -11,6 +11,7 @@ import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -66,7 +67,8 @@ public class Controller {
                 if (newValue != null) {
                     TodoItem item = todoListView.getSelectionModel().getSelectedItem();
                     itemDetailsTextArea.setText(item.getDetails());
-                    deadLineLabel.setText(item.getDeadline().toString());
+                    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+                    deadLineLabel.setText(dateTimeFormatter.format(item.getDeadline()));
                 }
             }
         });
