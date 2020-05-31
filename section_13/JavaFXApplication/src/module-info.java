3513e1fcc8b5298f01891b15051b9c7c2f5c7510
beltranbot <beltranbot@gmail.com>
2020-05-30 19:46:56 -0500

    213. more on choosers and web pages

--- section_13/JavaFXApplication/src/module-info.java
+++ section_13/JavaFXApplication/src/module-info.java
@@ -2,6 +2,8 @@ module JavaFXApplication {
 
     requires javafx.fxml;
     requires javafx.controls;
+    requires javafx.web;
+    requires java.desktop;
 
     opens sample;
 }
--- section_13/JavaFXApplication/src/sample/Controller.java
+++ section_13/JavaFXApplication/src/sample/Controller.java
@@ -5,10 +5,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
-import javafx.stage.DirectoryChooser;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.FileChooser;
 
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
 public class Controller {
 
@@ -18,6 +24,8 @@ public class Controller {
     private Button button4;
     @FXML
     private GridPane gridPane;
+    @FXML
+    private WebView webView;
 
     public void initialize() {
         button4.setEffect(new DropShadow());
@@ -37,14 +45,45 @@ public class Controller {
 
     @FXML
     public void handleClick() {
-//        FileChooser chooser = new FileChooser();
-        DirectoryChooser chooser = new DirectoryChooser();
-        File file = chooser.showDialog(gridPane.getScene().getWindow());
-        if (file != null) {
-            System.out.println(file.getPath());
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Save application file");
+        chooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text", "*.txt"),
+                new FileChooser.ExtensionFilter("PDF", "*.pdf"),
+                new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.png", "*.gif"),
+                new FileChooser.ExtensionFilter("All types", "*.*")
+        );
+        List<File> files = chooser.showOpenMultipleDialog(gridPane.getScene().getWindow());
+        if (files != null) {
+            for (File file : files) {
+                System.out.println(file.getPath());
+            }
         } else {
             System.out.println("Chooser was cancelled");
         }
-//        chooser.showOpenDialog(gridPane.getScene().getWindow());
     }
+
+    @FXML
+    public void handleLinkClick() {
+        String osName = System.getProperty("os.name");
+        String urlString = "http://www.javafx.com";
+
+//        try {
+//            if (osName.equals("Linux")) {
+//                if (Runtime.getRuntime().exec(new String[]{"which", "xdg-open"}).getInputStream().read() != -1) {
+//                    Runtime.getRuntime().exec(new String[]{"xdg-open", urlString});
+//                }
+//            } else {
+//                Desktop.getDesktop().browse(new URI(urlString));
+//            }
+//        } catch (IOException ioException) {
+//            ioException.printStackTrace();
+//        } catch (URISyntaxException uriSyntaxException) {
+//            uriSyntaxException.printStackTrace();
+//        }
+        WebEngine webEngine = webView.getEngine();
+        webEngine.load(urlString);
+    }
+
+
 }
