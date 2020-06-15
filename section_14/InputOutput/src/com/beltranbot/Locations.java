005945c6ce650dddda215928adb70b4d7de79587
beltranbot <beltranbot@gmail.com>
2020-06-14 21:28:42 -0500

    237. byte streams

--- section_14/InputOutput/src/com/beltranbot/Locations.java
+++ section_14/InputOutput/src/com/beltranbot/Locations.java
@@ -1,5 +1,6 @@
 package com.beltranbot;
 
+import java.awt.*;
 import java.io.*;
 import java.util.*;
 
@@ -10,15 +11,18 @@ public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new LinkedHashMap<>();
 
     public static void main(String[] args) throws IOException {
-        try (
-                BufferedWriter locationsFile = new BufferedWriter(new FileWriter("locations.txt"));
-                BufferedWriter dirFile = new BufferedWriter(new FileWriter("directions.txt"));
-        ) {
+        try (DataOutputStream locFile = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("locations.dat")))) {
             for (Location location : locations.values()) {
-                locationsFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
+                locFile.writeInt(location.getLocationID());
+                locFile.writeUTF(location.getDescription());
+                System.out.println("writing location " + location.getLocationID() + " :" + location.getDescription());
+                System.out.println("Writing " + (location.getExits().size() - 1) + " exits.");
+                locFile.writeInt(location.getExits().size() - 1);
                 for (String direction : location.getExits().keySet()) {
                     if (!direction.equalsIgnoreCase("Q")) {
-                        dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
+                        System.out.println("\t\t" + direction + ", " + location.getExits().get(direction));
+                        locFile.writeUTF(direction);
+                        locFile.writeInt(location.getExits().get(direction));
                     }
                 }
             }
