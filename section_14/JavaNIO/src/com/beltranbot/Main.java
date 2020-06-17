accf5675343ecd2a543712704f0a90058f1b7892
beltranbot <beltranbot@gmail.com>
2020-06-16 23:05:00 -0500

    248. reading files with nio

--- section_14/JavaNIO/src/com/beltranbot/Main.java
+++ section_14/JavaNIO/src/com/beltranbot/Main.java
@@ -2,6 +2,7 @@ package com.beltranbot;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.FileSystems;
@@ -34,9 +35,51 @@ public class Main {
             intBuffer.flip();
             numBytes = binChannel.write(intBuffer);
             System.out.println("numBytes written was: " + numBytes);
+
+//            readingBinaryFileUsingJavaIO(outputBytes);
+            RandomAccessFile randomAccessFile = new RandomAccessFile(FILENAME_DAT, "rwd");
+            FileChannel channel = randomAccessFile.getChannel();
+            outputBytes[0] = 'a';
+            outputBytes[1] = 'b';
+            buffer.flip();
+            long numBytesRead = channel.read(buffer);
+            if (buffer.hasArray()) {
+                System.out.println("byte buffer = " + new String(buffer.array()));
+            }
+            // Absolute read
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+            System.out.println(intBuffer.getInt(0));
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+            System.out.println(intBuffer.getInt(0));
+            // Relative read
+            //            intBuffer.flip();
+            //            numBytesRead = channel.read(intBuffer);
+            //            intBuffer.flip();
+            //            System.out.println(intBuffer.getInt());
+            //            intBuffer.flip();
+            //            numBytesRead = channel.read(intBuffer);
+            //            intBuffer.flip();
+            //            System.out.println(intBuffer.getInt());
+            //            channel.close();
+            //            randomAccessFile.close();
+
         } catch (IOException exception) {
             exception.printStackTrace();
         }
+    }
+
+    private static void readingBinaryFileUsingJavaIO(byte[] outputBytes) throws IOException{
+        RandomAccessFile randomAccessFile = new RandomAccessFile(FILENAME_DAT, "rwd");
+        byte[] b = new byte[outputBytes.length];
+        randomAccessFile.read(b);
+        System.out.println(new String(b));
+
+        long int1 = randomAccessFile.readInt();
+        long int2 = randomAccessFile.readInt();
+        System.out.println(int1);
+        System.out.println(int2);
     }
 
     private static void readingAndWritingWithJavaNIO() {
