f416945c50f6484180907ff6c27ed2e0cff9cd88
beltranbot <beltranbot@gmail.com>
2020-07-06 22:52:31 -0500

    255. exists and copy copyfile

--- section_14/Paths/src/Main.java
+++ section_14/Paths/src/Main.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 public class Main {
     private static final String WORKING_DIRECTORY_FILE = "WorkingDirectoryFile.txt";
@@ -11,23 +8,43 @@ public class Main {
     private static final String OUTTHERE_FILE = "OutThere.txt";
 
     public static void main(String[] args) {
-        Path path = FileSystems.getDefault().getPath(WORKING_DIRECTORY_FILE);
-        printFile(path);
+        try {
+            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
+            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+            Files.copy(sourceFile, copyFile);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
 
-//        Path filePath = FileSystems.getDefault().getPath("files", SUBDIRECTORY_FILE);
-        Path filePath = FileSystems.getDefault().getPath(".","files", SUBDIRECTORY_FILE);
-        printFile(filePath);
 
-//        filePath = Paths.get("/home/beltran/IdeaProjects/JavaMasterClass/section_14/" + OUTTHERE_FILE);
-        filePath = Paths.get("/home/beltran/IdeaProjects", "JavaMasterClass/section_14/", OUTTHERE_FILE);
-        printFile(filePath);
 
-        filePath = Paths.get(".");
-        System.out.println(filePath.toAbsolutePath());
-
-        Path path2 = FileSystems.getDefault().getPath(".", "files", "..", "files", SUBDIRECTORY_FILE);
-        System.out.println(path2.normalize().toAbsolutePath());
-        printFile(path2.normalize());
+//        Path path = FileSystems.getDefault().getPath(WORKING_DIRECTORY_FILE);
+//        printFile(path);
+//
+////        Path filePath = FileSystems.getDefault().getPath("files", SUBDIRECTORY_FILE);
+//        Path filePath = FileSystems.getDefault().getPath(".","files", SUBDIRECTORY_FILE);
+//        printFile(filePath);
+//
+////        filePath = Paths.get("/home/beltran/IdeaProjects/JavaMasterClass/section_14/" + OUTTHERE_FILE);
+//        filePath = Paths.get("/home/beltran/IdeaProjects", "JavaMasterClass/section_14/", OUTTHERE_FILE);
+//        printFile(filePath);
+//
+//        filePath = Paths.get(".");
+//        System.out.println(filePath.toAbsolutePath());
+//
+//        Path path2 = FileSystems.getDefault().getPath(".", "files", "..", "files", SUBDIRECTORY_FILE);
+//        System.out.println(path2.normalize().toAbsolutePath());
+//        printFile(path2.normalize());
+//
+//        Path path3 = FileSystems.getDefault().getPath("thisfiledoesntexist.txt");
+//        System.out.println(path3.toAbsolutePath());
+//
+//        Path path4 = Paths.get("/Volumes/Test/ING", "abcdef", "whatever.txt");
+//        System.out.println(path4.toAbsolutePath());
+//
+//        filePath = FileSystems.getDefault().getPath("files");
+//        System.out.println("Exists = " + Files.exists(filePath));
+//        System.out.println("Exists = " + Files.exists(path4  ));
 
     }
 
