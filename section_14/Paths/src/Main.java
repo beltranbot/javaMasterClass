e445bb9c93c27c3d6d3c4997ae53176988bc5026
beltranbot <beltranbot@gmail.com>
2020-07-06 23:12:50 -0500

    256. move, rename and delete

--- section_14/Paths/src/Main.java
+++ section_14/Paths/src/Main.java
@@ -9,11 +9,31 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
-            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
-            Files.copy(sourceFile, copyFile);
+
+            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+            Files.deleteIfExists(fileToDelete);
+
+
+            // rename file
+//            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1qqqq.txt");
+//            Path destination = FileSystems.getDefault().getPath("Examples", "file2.txt");
+//            Files.move(fileToMove, destination);
+
+//            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+//            Path destination = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+//            Files.move(fileToMove, destination);
+
+//            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+//
+//            sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
+//            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+
         } catch(IOException e) {
             e.printStackTrace();
+            System.out.println(e.getMessage());
         }
 
 
