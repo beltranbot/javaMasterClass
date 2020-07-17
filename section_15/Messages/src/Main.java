d5f6a01da09f2e5ef0d393ae2f9866b4b8c76bc6
beltranbot <beltranbot@gmail.com>
2020-07-17 01:23:03 -0500

    271. deadlocks, wait, notify and notifyAll methods

--- section_15/Messages/src/Main.java
+++ section_15/Messages/src/Main.java
@@ -3,10 +3,10 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
     }
-
-
 }
 
 class Message {
@@ -15,21 +15,32 @@ class Message {
 
     public synchronized String read() {
         while (empty) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
 
+            }
         }
         empty = true;
+        notifyAll();
         return message;
     }
 
     public synchronized void write(String message) {
         while (!empty) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
 
+            }
         }
         empty = false;
         this.message = message;
+        notifyAll();
     }
 }
 
+// producer
 class Writer implements Runnable {
     private Message message;
 
@@ -57,5 +68,28 @@ class Writer implements Runnable {
         }
 
         message.write("Finished");
+    }
+}
+
+// consumer
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+
+        for (String latestMessage = message.read(); !latestMessage.equals("Finished"); latestMessage = message.read()) {
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch(InterruptedException e) {
+
+            }
+        }
     }
 }
