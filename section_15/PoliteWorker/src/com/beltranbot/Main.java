5695796dbd03adac6b0c57ff708edacaf8be7d47
beltranbot <beltranbot@gmail.com>
2020-07-22 00:19:56 -0500

    281. fair locks and live locks

---
+++ section_15/PoliteWorker/src/com/beltranbot/Main.java
@@ -0,0 +1,20 @@
+package com.beltranbot;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        final Worker worker1 = new Worker("Worker 1", true);
+        final Worker worker2 = new Worker("Worker 2", true);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                
+            }
+        })
+
+
+    }
+}
