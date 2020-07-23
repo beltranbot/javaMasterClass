007a82990e542ce402ff983e7fb59902e354e5d3
beltranbot <beltranbot@gmail.com>
2020-07-23 00:06:30 -0500

    285. data binding

--- section_15/Task Example/src/sample/Controller.java
+++ section_15/Task Example/src/sample/Controller.java
@@ -5,27 +5,41 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 
 public class Controller {
 
     private Task<ObservableList<String>> task;
     @FXML
     private ListView listView;
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private Label progressLabel;
 
     public void initialize() {
         task = new Task<ObservableList<String>>() {
             @Override
             protected ObservableList<String> call() throws Exception {
                 Thread.sleep(1000);
-                final ObservableList<String> employees = FXCollections.observableArrayList(
+                String[] names = {
                         "Time Buchalka",
                         "Bill Rogers",
                         "Jack Jill",
                         "Joan Andrews",
                         "Mary Johnson",
                         "Bob McDonald"
-                );
+                };
+                ObservableList<String> employees = FXCollections.observableArrayList();
+
+                for (int i = 0; i < names.length; i++) {
+                    employees.add(names[i]);
+                    updateMessage("Added " + names[i] + " to the list");
+                    updateProgress(i + 1, names.length);
+                    Thread.sleep(200);
+                }
 
 //                Platform.runLater(new Runnable() {
 //                    @Override
@@ -38,6 +52,8 @@ public class Controller {
             }
         };
 
+        progressLabel.textProperty().bind(task.messageProperty());
+        progressBar.progressProperty().bind(task.progressProperty());
         listView.itemsProperty().bind(task.valueProperty());
     }
 
