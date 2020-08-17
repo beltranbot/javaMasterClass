04cd4fabac640ff2d753b7690ed837cf1941fc2d
beltranbot <beltranbot@gmail.com>
2020-08-16 23:55:51 -0500

    286. service

--- section_15/Task Example/src/sample/Controller.java
+++ section_15/Task Example/src/sample/Controller.java
@@ -1,9 +1,11 @@
 package sample;
 
-import javafx.application.Platform;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Service;
 import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -19,46 +21,27 @@ public class Controller {
     @FXML
     private Label progressLabel;
 
+    private Service<ObservableList<String>> service;
+
     public void initialize() {
-        task = new Task<ObservableList<String>>() {
-            @Override
-            protected ObservableList<String> call() throws Exception {
-                Thread.sleep(1000);
-                String[] names = {
-                        "Time Buchalka",
-                        "Bill Rogers",
-                        "Jack Jill",
-                        "Joan Andrews",
-                        "Mary Johnson",
-                        "Bob McDonald"
-                };
-                ObservableList<String> employees = FXCollections.observableArrayList();
 
-                for (int i = 0; i < names.length; i++) {
-                    employees.add(names[i]);
-                    updateMessage("Added " + names[i] + " to the list");
-                    updateProgress(i + 1, names.length);
-                    Thread.sleep(200);
-                }
+        service = new EmployeeService();
+        progressLabel.textProperty().bind(service.messageProperty());
+        progressBar.progressProperty().bind(service.progressProperty());
+        listView.itemsProperty().bind(service.valueProperty());
 
-//                Platform.runLater(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        listView.setItems(employees);
-//                    }
-//                });
-
-                return employees;
-            }
-        };
-
-        progressLabel.textProperty().bind(task.messageProperty());
-        progressBar.progressProperty().bind(task.progressProperty());
-        listView.itemsProperty().bind(task.valueProperty());
+        progressBar.visibleProperty().bind(service.runningProperty());
+        progressLabel.visibleProperty().bind(service.runningProperty());
     }
 
     @FXML
     public void buttonPressed() {
-        new Thread(task).start();
+        if (service.getState() == Service.State.SUCCEEDED) {
+            service.reset();
+            service.start();
+        } else if (service.getState() == Service.State.READY) {
+            service.start();
+        }
+
     }
 }
