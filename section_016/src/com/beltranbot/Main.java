e67f3a72b088aa925f7b9a775b111426ba6a2c42
beltranbot <beltranbot@gmail.com>
2020-10-19 00:11:45 -0500

    293. lambda expression continued

--- section_016/src/com/beltranbot/Main.java
+++ section_016/src/com/beltranbot/Main.java
@@ -2,6 +2,7 @@ package com.beltranbot;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -23,8 +24,9 @@ public class Main {
         employeeList.add(tim);
         employeeList.add(jack);
         employeeList.add(snow);
-        Collections.sort(employeeList, (e1, e2) -> e1.getName().compareTo(e2.getName()));
-
+        Collections.sort(employeeList, Comparator.comparing(Employee::getName));
+//        Collections.sort(employeeList, (e1, e2) -> e1.getName().compareTo(e2.getName()));
+//
 //        Collections.sort(emploeList, new Comparator<Employee>() {
 //            @Override
 //            public int compare(Employee o1, Employee o2) {
@@ -37,6 +39,28 @@ public class Main {
         for (Employee employee : employeeList) {
             System.out.println(employee.getName());
         }
+
+        UpperConcat uc = (s1, s2) -> s1.toUpperCase() + s2.toUpperCase();
+        String sillyString = doStringStuff(
+                uc,
+                employeeList.get(0).getName(),
+                employeeList.get(1).getName()
+        );
+
+//        String sillyString = doStringStuff(new UpperConcat() {
+//            @Override
+//            public String upperAndConcat(String s1, String s2) {
+//                return s1.toUpperCase() + s2.toUpperCase();
+//            }
+//        },
+//                employeeList.get(0).getName(),
+//                employeeList.get(1).getName()
+//        );
+        System.out.println(sillyString);
+    }
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
     }
 }
 
@@ -64,4 +88,8 @@ class Employee {
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface UpperConcat {
+    String upperAndConcat(String s1, String s2);
 }
