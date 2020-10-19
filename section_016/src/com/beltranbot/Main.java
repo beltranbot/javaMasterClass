ab05b6f7aede22d25ca0af9ea8c5eab4a9773ee0
beltranbot <beltranbot@gmail.com>
2020-10-18 23:40:22 -0500

    292. lambda expression introduction

---
+++ section_016/src/com/beltranbot/Main.java
@@ -0,0 +1,67 @@
+package com.beltranbot;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            System.out.println("Printing from the runnable");
+            System.out.println("Line 2");
+            System.out.format("This is line %d\n", 3);
+        }).start();
+
+        Employee john = new Employee("John Doe", 30);
+        Employee tim = new Employee("Tim Buchalka", 21);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(john);
+        employeeList.add(tim);
+        employeeList.add(jack);
+        employeeList.add(snow);
+        Collections.sort(employeeList, (e1, e2) -> e1.getName().compareTo(e2.getName()));
+
+//        Collections.sort(emploeList, new Comparator<Employee>() {
+//            @Override
+//            public int compare(Employee o1, Employee o2) {
+//                return o1.getName().compareTo(o2.getName());
+//            }
+//});
+
+//        Collections.sort(employeeList, ());
+//
+        for (Employee employee : employeeList) {
+            System.out.println(employee.getName());
+        }
+    }
+}
+
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
