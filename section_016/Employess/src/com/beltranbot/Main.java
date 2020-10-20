79a8b68d6c83ffd4c84eb08ad683cb8a3fee3913
beltranbot <beltranbot@gmail.com>
2020-10-19 21:47:34 -0500

    297. more on predicates & suppliers

--- section_016/Employess/src/com/beltranbot/Main.java
+++ section_016/Employess/src/com/beltranbot/Main.java
@@ -2,7 +2,10 @@ package com.beltranbot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Main {
 
@@ -32,7 +35,29 @@ public class Main {
                 "Employees 30 and under",
                 employee -> employee.getAge() <= 30
         );
+        printEmployeesByAge(
+                employeeList,
+                "Employees under 24",
+                new Predicate<Employee>() {
+                    @Override
+                    public boolean test(Employee employee) {
+                        return employee.getAge() < 25;
+                    }
+                }
+        );
+        IntPredicate greaterThanFifteen= i -> i > 15;
+        IntPredicate lessThan100 = i -> i < 100;
+        System.out.println(greaterThanFifteen.test(10));
+        int a = 20;
+        System.out.println(greaterThanFifteen.test(a + 5));
+        System.out.println(greaterThanFifteen.and(lessThan100).test(50));
+        System.out.println(greaterThanFifteen.and(lessThan100).test(15));
 
+        Random random = new Random();
+        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(randomSupplier.get());
+        }
     }
 
     private static void printEmployeesByAge(
