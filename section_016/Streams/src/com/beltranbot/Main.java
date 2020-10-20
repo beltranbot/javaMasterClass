001304ae4a93f9d8d7cbe4d5809c559329d33148
beltranbot <beltranbot@gmail.com>
2020-10-19 23:36:12 -0500

    302. streams -faltmap & lambda best practices

--- section_016/Streams/src/com/beltranbot/Main.java
+++ section_016/Streams/src/com/beltranbot/Main.java
@@ -3,6 +3,8 @@ package com.beltranbot;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -33,6 +35,77 @@ public class Main {
                         .peek(System.out::println)
                         .count()
         );
+
+        Employee john = new Employee("John Doe", 30);
+        Employee jane = new Employee("Jane Deer", 25);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+        Employee test = new Employee("Engineer", 25);
+
+        Department engineering = new Department("engineering");
+        engineering.addEmployee(test);
+
+        Department hr = new Department("Human Resources");
+        hr.addEmployee(jane);
+        hr.addEmployee(jack);
+        hr.addEmployee(snow);
+        Department accounting = new Department("Accounting");
+        accounting.addEmployee(john);
+
+        List<Department> departments = new ArrayList<>();
+        departments.add(hr);
+        departments.add(accounting);
+        departments.add(engineering);
+
+        departments
+                .stream()
+                .flatMap(department -> department.getEmployeeList().stream())
+                .forEach(System.out::println);
+
+        System.out.println("_______________________");
+//        List<String> sortedGNumbers = someBingoNumbers
+//                .stream()
+//                .map(String::toUpperCase)
+//                .filter(s -> s.startsWith("G"))
+//                .sorted()
+//                .collect(Collectors.toList());
+        List<String> sortedGNumbers = someBingoNumbers
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        for (String s : sortedGNumbers) {
+            System.out.println(s);
+        }
+
+        Map<Integer, List<Employee>> groupedByAge = departments.stream()
+                .flatMap(department -> department.getEmployeeList().stream())
+                .collect(Collectors.groupingBy(employee -> employee.getAge()));
+
+        System.out.println("---------- grouped by age -------------");
+        for (List<Employee> employeeList : groupedByAge.values()) {
+            for (Employee employee : employeeList) {
+                System.out.println(employee);
+            }
+        }
+
+        System.out.println("---------- youngest employee --------");
+
+        departments.stream()
+                .flatMap(department -> department.getEmployeeList().stream())
+                .reduce((e1, e2) -> e1.getAge() < e2.getAge() ? e1 : e2)
+                .ifPresent(System.out::println);
+
+        Stream.of("ABC", "AC", "BAA", "CCCC", "XY", "ST")
+                .filter(s -> {
+                    System.out.println(s);
+                    return s.length() == 3;
+                })
+                .count();
+
+
+
 
 
 //        someBingoNumbers.forEach(number -> {
