869156ecc0318af7458eb836a444b5e29f1b211d
beltranbot <beltranbot@gmail.com>
2020-10-20 20:54:26 -0500

    304. lambda challenge part 2

--- section_016/Challenges/src/com/beltranbot/Main.java
+++ section_016/Challenges/src/com/beltranbot/Main.java
@@ -1,5 +1,7 @@
 package com.beltranbot;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -22,6 +24,30 @@ public class Main {
         Supplier<String> iLoveJava = () -> "I love Java!";
         String supplierResult = iLoveJava.get();
         System.out.println(supplierResult);
+
+        List<String> topNames2015 = Arrays.asList(
+                "Amelia",
+                "Olivia",
+                "emily",
+                "Isla",
+                "Ava",
+                "oliver",
+                "Jack",
+                "Charlie",
+                "harry",
+                "Jacob"
+        );
+        topNames2015
+                .stream()
+                .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
+                .sorted()
+                .forEach(System.out::println);
+
+        long number = topNames2015
+                .stream()
+                .filter(s -> s.toUpperCase().startsWith("A"))
+                .count();
+        System.out.println("Number of names that start with A: " + number);
     }
 
     private static void challenge1() {
@@ -32,7 +58,6 @@ public class Main {
                 String[] parts = myString.split(" ");
                 for (String part : parts) {
                     System.out.println(part);
-
                 }
             }
         };
