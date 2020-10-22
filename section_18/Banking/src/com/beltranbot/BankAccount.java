afbbe5e06623909e54e8010254b0f2bc512c6c62
beltranbot <beltranbot@gmail.com>
2020-10-21 21:19:55 -0500

    318. asserts in junit

--- section_18/Banking/src/com/beltranbot/BankAccount.java
+++ section_18/Banking/src/com/beltranbot/BankAccount.java
@@ -6,10 +6,16 @@ public class BankAccount {
     private String lastName;
     private double balance;
 
-    public BankAccount(String firstName, String lastName, double balance) {
+    public static final int CHECKING = 1;
+    public static final int SAVINGS = 2;
+
+    private int accountType;
+
+    public BankAccount(String firstName, String lastName, double balance, int accountType) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.balance = balance;
+        this.accountType = accountType;
     }
     
     // The branch argument is true if the customer is performing the traensaction
@@ -24,12 +30,16 @@ public class BankAccount {
     // at a branch, with a teller.
     // It's false if the customer is performing the transaction at an ATM
     public double withdraw(double amount, boolean branch) {
-        balance += amount;
+        balance -= amount;
         return balance;
     }
 
     public double getBalance() {
         return balance;
+    }
+
+    public boolean isChecking() {
+        return accountType == CHECKING;
     }
 
     // More methods that use firstName, lastName, and perform other functions
