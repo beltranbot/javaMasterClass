5dfc782d76a7c341658347fa7aa0bb1a9e36cae7
beltranbot <beltranbot@gmail.com>
2020-10-21 21:46:53 -0500

    319. more asserts and exception handling

--- section_18/Banking/src/com/beltranbot/BankAccountTest.java
+++ section_18/Banking/src/com/beltranbot/BankAccountTest.java
@@ -3,36 +3,63 @@ package com.beltranbot;
 import static org.junit.Assert.*;
 
 public class BankAccountTest {
+    private BankAccount account;
+    private static int count = 0;
+
+    @org.junit.BeforeClass
+    public static void beforeClass() {
+        System.out.println("This executes before any of the test cases. Count = " + count++);
+    }
+
+    @org.junit.Before
+    public void setup() {
+        account = new BankAccount("Time", "Buchalka", 1000.0, BankAccount.CHECKING);
+        System.out.println("Running a test...");
+        System.out.println("Count = " + count++);
+    }
 
     @org.junit.Test
     public void deposit() {
-        BankAccount account = new BankAccount("Time", "Buchalka", 1000.00, BankAccount.CHECKING);
         double balance = account.deposit(200.00, true);
         assertEquals(1200, balance, 0);
     }
 
     @org.junit.Test
-    public void withdraw() {
-        fail("This test has yet to be implemented");
+    public void withdraw_branch() {
+        double balance = account.withdraw(600.0, true);
+        assertEquals(400.0, balance, 0);
+    }
+
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void withdraw_notBranch() {
+        double balance = account.withdraw(600.0, false);
+        assertEquals(400.0, balance, 0);
     }
 
     @org.junit.Test
     public void getBalance_deposit() {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
         account.deposit(200.00, true);
         assertEquals(1200, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void getBalance_withdraw() {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
         account.withdraw(200.00, true);
         assertEquals(800, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void isChecking_true() {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
         assertTrue("The account is NOT a checking account", account.isChecking());
+    }
+
+    @org.junit.AfterClass
+    public static void afterClass() {
+        System.out.println("This executes after any test cases. Count = " + count++);
+    }
+
+    @org.junit.After
+    public void teardown() {
+        System.out.println("Count = " + count++);
     }
 }
