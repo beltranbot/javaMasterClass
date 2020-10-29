081958cc9b7204f8029f052de7103955217bcc28
beltranbot <beltranbot@gmail.com>
2020-10-28 22:53:15 -0500

    338. jdbc insert, update, delete

--- section_19/TestDB/src/com/beltranbot/Main.java
+++ section_19/TestDB/src/com/beltranbot/Main.java
@@ -1,9 +1,6 @@
 package com.beltranbot;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
@@ -11,7 +8,32 @@ public class Main {
         try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:/home/beltran/IdeaProjects/JavaMasterClass/section_19/TestDB/testjava.db");
             Statement statement = conn.createStatement();
-            statement.execute("create table contacts (name text, phone integer, email text)");
+//            conn.setAutoCommit(false);
+            statement.execute("create table if not exists contacts" +
+                    " (name text, phone integer, email text)");
+//            statement.execute("insert into contacts (name, phone, email) " +
+//                    "values ('Joe', 45362, 'joe@anywhere.com')");
+//            statement.execute("insert into contacts (name, phone, email) " +
+//                    "values ('Jane', 4829484, 'jane@anywhere.com')");
+//            statement.execute("insert into contacts (name, phone, email) " +
+//                    "values ('Fido', 9038, 'dog@anywhere.com')");
+
+//            statement.execute("update contacts " +
+//                    "set phone = 5566789 " +
+//                    "where name='Jane'");
+
+//            statement.execute("delete from contacts " +
+//                    "where name = 'Joe'");
+
+            statement.execute("select * from contacts");
+            ResultSet results = statement.getResultSet();
+            while (results.next()) {
+                System.out.println(results.getString("name") + " " +
+                        results.getInt("phone") + " " +
+                        results.getString("email")
+                );
+            }
+            results.close();
             statement.close();
             conn.close(); // order is important
         } catch (SQLException ex) {
